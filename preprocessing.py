eccdcbd9af37cf67020bcf1c05c49120f06e98a5
Adam Fragkiadakis <adamfrag@gmail.com>
2023-04-06 16:48:33 +0200

    Create boundary boxes for the masks, fix a potential bug...

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,25 @@
 # Import necessary files and libraries.
 import os
 from shutil import copy
+
+import numpy
 import numpy as np
 from subprocess import run
 import SimpleITK as sITK
 import nibabel as nib
 from utils import setup_parser, validate_paths, create_output_structures
+
+
+# Copy files from the origin directory to the destination directory.
+def copy_files(input_dir, output_dir):
+    for patient in os.listdir(input_dir):
+        patient_dir = os.path.join(input_dir, patient)
+        for study in os.listdir(patient_dir):
+            study_dir = os.path.join(patient_dir, study)
+            for image in os.listdir(study_dir):
+                img_input = os.path.join(study_dir, image)
+                img_output = os.path.join(output_dir, patient, study, image)
+                copy(img_input, img_output)
 
 
 # Traverse through the given dataset paths and create paired paths between the available modalities.
@@ -86,73 +100,87 @@ def check_for_duplicates(input_dir, patients):
 
 
 # Perform a bias field correction for the MRIs.
-def bias_field_correction(input_mris, output_mris):
-    run(["clitkN4BiasFieldCorrection", "-i", input_mris["volume"], "-o", output_mris["volume"]])
+def bias_field_correction(mri):
+    run(["clitkN4BiasFieldCorrection", "-i", mri, "-o", mri])
 
 
 # Bring all the images of a patient into the same physical space of the chosen one.
-def resample(input_pairs, output_pairs, like_modality):
+def resample(pair, like_modality):
     # Get the image that gives the physical space reference.
-    like_img = input_pairs[like_modality]["volume"]
+    like_img = pair[like_modality]["volume"]
     # Get the images of the study that are about to be resampled.
     study = "CT" if like_modality == "ceMRI" else "MRI"
-    images = input_pairs[study]
+    images = pair[study]
 
     print(f"\t-Bring {study} in the physical space of {like_modality}")
     # Resample the images and store them in the respective output directory.
     for image in images:
-        img_input, img_output = input_pairs[study][image], output_pairs[study][image]
-        print(f"\t\t-Image: {img_input} ---> {img_output}")
-        run(["clitkAffineTransform", "-i", img_input, "-o", img_output, "-l", like_img])
-
-    # This copying mechanism is only needed if the resampling process is the first that takes place on the original
-    # nifty data. In any other case, the reference modality images will be available in the processed directory so
-    # this mechanism should be commented out.
-    # print(f"\t-Copying {like_modality} images...")
-    # for image in input_pairs[like_modality]:
-    #     img_input, img_output = input_pairs[like_modality][image], output_pairs[like_modality][image]
-    #     print(f"\t\t-Image: {img_input} ---> {img_output}")
-    #     copy(img_input, img_output)
+        img_path = pair[study][image]
+        print(f"\t\t-Image: {img_path}")
+        run(["clitkAffineTransform", "-i", img_path, "-o", img_path, "-l", like_img])
 
 
 # For all the available study pairs, adjust to minimum or predefined spacing for each axis (in mm).
-def change_spacing(input_pairs, output_pairs, spacing=None):
+def change_spacing(pair, spacing=None):
     # If there is not a predefined spacing, get the minimum spacing for each axis between the volumes
     if not spacing:
-        spacing = find_minimum(input_pairs["CT"]["volume"], input_pairs["MRI"]["volume"])
+        spacing = find_minimum(pair["CT"]["volume"], pair["MRI"]["volume"])
 
-    for study in input_pairs:
+    for study in pair:
         print(f"\t-Study: {study}")
-        for image in input_pairs[study]:
-            img_input, img_output = input_pairs[study][image], output_pairs[study][image]
-            print(f"\t\t-Image: {img_input} ---> {img_output}")
+        for image in pair[study]:
+            img_path = pair[study][image]
+            print(f"\t\t-Image: {img_path}")
 
-            run(["clitkAffineTransform", "-i", img_input, "-o", img_output, "--adaptive",
+            run(["clitkAffineTransform", "-i", img_path, "-o", img_path, "--adaptive",
                  f"--spacing={str(spacing[0])},{str(spacing[1])},{str(spacing[2])}"])
 
 
 # Crop the pairs. For each study, crop automatically the mask and based on the mask, crop the volume.
-def crop(input_pairs, output_pairs):
-    for study in input_pairs:
+def crop(pair):
+    for study in pair:
         print(f"\t-Study: {study}")
 
         # Crop the liver mask automatically.
-        liver_input = input_pairs[study]["liver"]
-        liver_output = output_pairs[study]["liver"]
+        liver_path = pair[study]["liver"]
 
-        print(f"\t\t-Nifty: {liver_input} ---> {liver_output}")
-        run(["clitkAutoCrop", "-i", liver_input, "-o", liver_output])
+        print(f"\t\t-Nifty: {liver_path}")
+        run(["clitkAutoCrop", "-i", liver_path, "-o", liver_path])
 
         # Crop the volume and the tumor mask according to the cropped liver mask.
-        volume_input = input_pairs[study]["volume"]
-        volume_output = output_pairs[study]["volume"]
-        tumor_input = input_pairs[study]["tumor"]
-        tumor_output = output_pairs[study]["tumor"]
+        volume_path = pair[study]["volume"]
+        tumor_path = pair[study]["tumor"]
 
-        print(f"\t\t-Nifty: {volume_input} ---> {volume_output}")
-        run(["clitkCropImage", "-i", volume_input, "--like", liver_output, "-o", volume_output])
-        print(f"\t\t-Nifty: {tumor_input} ---> {tumor_output}")
-        run(["clitkCropImage", "-i", tumor_input, "--like", liver_output, "-o", tumor_output])
+        print(f"\t\t-Nifty: {volume_path}")
+        run(["clitkCropImage", "-i", volume_path, "--like", liver_path, "-o", volume_path])
+        print(f"\t\t-Nifty: {tumor_path}")
+        run(["clitkCropImage", "-i", tumor_path, "--like", liver_path, "-o", tumor_path])
+
+
+# For each mask, create a boundary box that surrounds the mask.
+def create_bounding_boxes(pair):
+    for study in pair:
+        print(f"\t-Study: {study}")
+        for mask in [pair[study]["liver"], pair[study]["tumor"]]:
+            print(f"\t\t-Mask: {mask}")
+            # Load the mask and convert it into a numpy array.
+            mask_nii = nib.load(mask)
+            mask_data = np.array(mask_nii.get_fdata())
+
+            # Get the segmentation and find min and max for each axis.
+            segmentation = np.where(mask_data == 1)
+            x_min, x_max = int(np.min(segmentation[0])), int(np.max(segmentation[0]))
+            y_min, y_max = int(np.min(segmentation[1])), int(np.max(segmentation[1]))
+            z_min, z_max = int(np.min(segmentation[2])), int(np.max(segmentation[2]))
+
+            # Create bounding box.
+            bounding_box = numpy.zeros(mask_data.shape)
+            bounding_box[x_min:x_max, y_min:y_max, z_min:z_max] = 1
+
+            # Convert to nifty and save the image.
+            bounding_box = nib.Nifti1Image(bounding_box, affine=mask_nii.affine)
+            split = mask.split(".")
+            nib.save(bounding_box, f"{split[0]}_bb.{split[1]}")
 
 
 def main():
@@ -168,25 +196,29 @@ def main():
     # Create the output respective structures.
     create_output_structures(input_dir, output_dir, depth=2)
 
+    # Copy files from the origin directory.
+    copy_files(input_dir, output_dir)
+
     # Make a check to handle any possible duplicate data.
-    # check_for_duplicates(input_dir, os.listdir(input_dir))
+    # check_for_duplicates(output_dir, os.listdir(output_dir))
 
     # Do the required preprocessing for each of the patients.
-    for patient in os.listdir(input_dir):
-        patient_input, patient_output = os.path.join(input_dir, patient), os.path.join(output_dir, patient)
-        input_studies = [study for study in os.listdir(patient_input) if study == "ceMRI" or study == ct_study]
-        output_studies = [study for study in os.listdir(patient_input) if study == "ceMRI" or study == ct_study]
-        input_pair = get_pair_paths(patient_input, input_studies)
-        output_pair = get_pair_paths(patient_output, output_studies)
+    for patient in os.listdir(output_dir):
+        patient_dir = os.path.join(output_dir, patient)
+        studies_dir = [study for study in os.listdir(patient_dir) if study == "ceMRI" or study == ct_study]
+
+        pair = get_pair_paths(patient_dir, studies_dir)
 
         # print(f"-Bias field correction for patient: {patient}")
-        # bias_field_correction(input_pair["MRI"], output_pair["MRI"])
+        # bias_field_correction(pair["MRI"]["volume"])
         # print(f"\n-Adjust Spacing for patient: {patient}")
-        # change_spacing(output_pair, output_pair)
-        # print(f"\n-Resampling for patient: {patient}")
-        # resample(output_pair, output_pair, rs_reference)
+        # change_spacing(pair)
+        print(f"\n-Resampling for patient: {patient}")
+        resample(pair, rs_reference)
         # print(f"\n-Cropping for patient: {patient}")
-        # crop(output_pair, output_pair)
+        # crop(pair)
+        print(f"\n-Create boundary boxes for patient: {patient}")
+        create_bounding_boxes(pair)
 
 
 # Use this file as a script and run it.
